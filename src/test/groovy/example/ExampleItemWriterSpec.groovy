0eca10c4dc7f878a6f5cdb2360b3879249be02a6
amr <dquijote22@gmail.com>
2015-05-13 12:32:47 +0200

    Initial commit with an example job execution

---
+++ src/test/groovy/example/ExampleItemWriterSpec.groovy
@@ -0,0 +1,16 @@
+package example
+
+import spock.lang.Specification
+
+class ExampleItemWriterSpec extends Specification {
+
+    void 'write outputs'()  {
+        given:
+            def writer = new ExampleItemWriter()
+
+        expect: "nothing bad happens when writing arbitrary objects"
+            writer.write(["hello", "workd"])
+            writer.write([42, new Object()])
+            writer.write(null)
+    }
+}
